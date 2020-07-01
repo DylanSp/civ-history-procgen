54f6e77a6ca98c12ef1322d24404710db6f3e344
Dylan Sprague <dsprague@docuphase.com>
2020-07-01 17:41:52 -0400

    Add console app for generating, displaying grid - untested

--- CivilizationHistoryProcGen/Program.cs
+++ CivilizationHistoryProcGen/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Terrain;
 
 namespace CivilizationHistoryProcGen
 {
@@ -6,7 +7,42 @@ namespace CivilizationHistoryProcGen
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DisplayGrid();
+
+            Console.WriteLine("Press Enter to exit");
+            Console.ReadLine();
+        }
+
+        private static void DisplayGrid()
+        {
+            var terrainGenerator = new TerrainGenerator();
+            var gridLength = 10;
+            var grid = terrainGenerator.GenerateGrid(gridLength);
+
+            for (var row = 0; row < grid.GetLength(0); row++)
+            {
+                for (var col = 0; col < grid.GetLength(1); col++)
+                {
+                    switch (grid[row, col])
+                    {
+                        case Ocean _: 
+                            Console.Write("o");
+                            break;
+                        case Plains _:
+                            Console.Write("p");
+                            break;
+                        case Hills _:
+                            Console.Write("H");
+                            break;
+                        case Mountains _:
+                            Console.Write("^");
+                            break;
+                    }
+                }
+                Console.WriteLine();
+            }
+             
+            Console.WriteLine();
         }
     }
 }
